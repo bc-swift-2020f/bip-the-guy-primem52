f70700536e0053f7ad5af1f1d2c38c81f608bbda
Morgan Prime <primem@bc.edu>
2020-09-20 23:16:14 -0400

    Initial Commit

---
+++ Bip the Guy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Bip the Guy
+//
+//  Created by Morgan Prime on 9/20/20.
+//  Copyright © 2020 Morgan Prime. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
